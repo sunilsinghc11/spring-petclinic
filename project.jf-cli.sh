90ad1fa8f941411f6ed978599915c02f2c11640d
Krishna Manchikalapudi <krishnam@jfrog.com>
2024-09-04 18:01:24 -0700

    rbv2 promotion condition

--- project.jf-cli.sh
+++ project.jf-cli.sh
@@ -5,7 +5,7 @@ clear
 # jf c add --user=krishnam --interactive=true --url=https://psazuse.jfrog.io --overwrite=true 
 
 # Config - Artifactory info
-export JF_RT_URL="https://psazuse.jfrog.io" JFROG_NAME="psazuse" JFROG_RT_USER="krishnam" JFROG_CLI_LOG_LEVEL="DEBUG" # JF_BEARER_TOKEN="<GET_YOUR_OWN_KEY>"
+export JF_RT_URL="https://psazuse.jfrog.io" JFROG_NAME="psazuse" JFROG_RT_USER="krishnam" JFROG_CLI_LOG_LEVEL="DEBUG" # JF_ACCESS_TOKEN="<GET_YOUR_OWN_KEY>"
 export RT_PROJECT_REPO="krishnam-mvn"
 
 echo " JFROG_NAME: $JFROG_NAME \n JF_RT_URL: $JF_RT_URL \n JFROG_RT_USER: $JFROG_RT_USER \n JFROG_CLI_LOG_LEVEL: $JFROG_CLI_LOG_LEVEL \n "
@@ -13,7 +13,7 @@ echo " JFROG_NAME: $JFROG_NAME \n JF_RT_URL: $JF_RT_URL \n JFROG_RT_USER: $JFROG
 # MVN 
 ## Config - project
 ### CLI
-export BUILD_NAME="spring-petclinic-rbv2" BUILD_ID="cmd.$(date '+%Y-%m-%d-%H-%M')" PACKAGE_CATEGORY="WEBAPP" RT_PROJECT_RB_SIGNING_KEY="krishnam"
+export BUILD_NAME="spring-petclinic-rbv2-pcondition" BUILD_ID="cmd.$(date '+%Y-%m-%d-%H-%M')" PACKAGE_CATEGORY="WEBAPP" RT_PROJECT_RB_SIGNING_KEY="krishnam"
 
 ### Jenkins
 # export BUILD_NAME=${env.JOB_NAME} BUILD_ID=${env.BUILD_ID} PACKAGE_CATEGORY="WEBAPP"
@@ -52,15 +52,44 @@ echo "\n" && cat rbv2-spec-${BUILD_ID}.json && echo "\n"
   # create RB to state=NEW
 jf rbc ${BUILD_NAME} ${BUILD_ID} --sync="true" --access-token="${JF_ACCESS_TOKEN=}" --url="${JF_RT_URL}" --signing-key="${RT_PROJECT_RB_SIGNING_KEY}" --spec="rbv2-spec-${BUILD_ID}.json" --spec-vars="build_name=${BUILD_NAME};build_id=${BUILD_ID};PACKAGE_CATEGORY=${PACKAGE_CATEGORY};state=new" --server-id="psazuse" 
 
+rm -rf rbv2-spec-${BUILD_ID}.json
 ## RBv2: release bundle - DEV promote
+echo "\n\n**** RBv2: Promoted to DEV ****\n\n"
 jf rbp --sync="true" --access-token="${JF_ACCESS_TOKEN=}" --url="${JF_RT_URL}" --signing-key="${RT_PROJECT_RB_SIGNING_KEY}" --server-id="psazuse" ${BUILD_NAME} ${BUILD_ID} DEV 
 
-## AQL: if state=NEW, do not promote to QA
-# jf rt curl -XGET /lifecycle/api/v2/release_bundle/records/${BUILD_NAME}/${BUILD_ID}
-#jf curl -X GET /lifecycle/api/v2/release_bundle/records/spring-petclinic-rbv2/cmd.2024-08-30-21-17
+sleep 5
 
-## RBv2: release bundle - QA promote
-# jf rbp --sync=true --url="${JF_RT_URL}" --access-token="${JF_BEARER_TOKEN}" --signing-key="${RT_PROJECT_RB_SIGNING_KEY}" ${BUILD_NAME} ${BUILD_ID} QA
+echo "\n\n**** Get BuilfInfo's Package category & RBv2 state ****\n\n"
+## AQL: if RBv2=DEV & BuildInfo package category = Web, promote to PROD else promote to QA
+#BP_RESP_DATA=$(curl --location '${JF_RT_URL}/artifactory/api/build/${BUILD_NAME}/${BUILD_ID}' --header 'Content-Type:  application/json' --header 'Authorization: Bearer ${JF_ACCESS_TOKEN}'  | jq -r '.buildInfo.properties')
+export BUILDINFO_PACKAGE_CATEGORY=$(jf rt curl /api/build/${BUILD_NAME}/${BUILD_ID}?async=false | jq -r '.buildInfo.properties."buildInfo.env.PACKAGE_CATEGORY"')
+# jf rt curl /api/build/spring-petclinic-rbv2/cmd.2024-08-30-21-17 | jq -r '.buildInfo.properties."buildInfo.env.PACKAGE_CATEGORY"'
+echo " BuildInfo Package Category =  $BUILDINFO_PACKAGE_CATEGORY \n"
+
+# `curl -v -G '${JF_RT_URL}/lifecycle/api/v2/promotion/records/${BUILD_NAME}/${BUILD_ID}?async=false' -H 'Content-Type:  application/json' -H 'Authorization: Bearer ${JF_ACCESS_TOKEN}'` > RB2_STATUS.txt
+
+# export RB2_STATUS=`cat RB2_STATUS.txt`
+# export RB2_STATUS=$(echo $RB2_STATUS | jq -r '.promotions[0].environment')
+echo "JF_ACCESS_TOKEN = ${JF_ACCESS_TOKEN}"
+
+export RB2_STATUS_RESP=$(curl -v -G https://psazuse.jfrog.io/lifecycle/api/v2/promotion/records/${BUILD_NAME}/${BUILD_ID}?async=false -H 'Content-Type:  application/json' -H "Authorization: Bearer ${JF_ACCESS_TOKEN}")
+
+echo $RB2_STATUS_RESP > RB2_STATUS.txt
+
+export RB2_STATUS=$(echo $RB2_STATUS_RESP | jq -r '.promotions[0].environment')
+echo " Release Bundle state =  $RB2_STATUS "
+
+# rm -rf RB2_STATUS.txt
+if [[ -n $BUILDINFO_PACKAGE_CATEGORY ]] ; then
+  if  [[ "WEBAPP" == "${BUILDINFO_PACKAGE_CATEGORY}" ]] && [[ "DEV" == "${RB2_STATUS}" ]] ; then 
+    echo "\n\n**** RBv2: Promoted to NEW --> DEV --> PROD ****\n\n"
+    jf rbp --sync="true" --access-token="${JF_ACCESS_TOKEN=}" --url="${JF_RT_URL}" --signing-key="${RT_PROJECT_RB_SIGNING_KEY}" --server-id="psazuse" ${BUILD_NAME} ${BUILD_ID} PROD  
+  else 
+    echo "\n\n**** RBv2: Promoted to NEW --> DEV --> QA ****\n\n"
+    jf rbp --sync="true" --access-token="${JF_ACCESS_TOKEN=}" --url="${JF_RT_URL}" --signing-key="${RT_PROJECT_RB_SIGNING_KEY}" --server-id="psazuse" ${BUILD_NAME} ${BUILD_ID} QA  
+  fi
+fi
+
 
 
 echo "\n\n**** DONE ****\n\n"
